b3b899c4bc60e9d63bb56b018e8d6319488c6544
Every <christiansantoslima21@gmail.com>
2023-09-02 22:49:20 -0300

    refactor: improve code redability and use smart pointers

--- Fibonacci heap/src/Fibonacci heap.cpp
+++ Fibonacci heap/src/Fibonacci heap.cpp
@@ -1,353 +1,316 @@
 #include <iostream>
 #include <cmath>
+#include <memory>
 
 struct Node {
-    int n;
+    int key;
     int degree;
-    Node* parent;
-    Node* child;
-    Node* left;
-    Node* right;
+    std::shared_ptr<Node> parent;
+    std::shared_ptr<Node> child;
+    std::shared_ptr<Node> left;
+    std::shared_ptr<Node> right;
     char mark;
-    char C;
+    char childCut;
 };
 
 class FibonacciHeap {
 
-private:
-    int nH;
-    Node* H;
-
 public:
 
     FibonacciHeap() {
-        H =  initializeHeap();
+        heapRoot = initializeHeap();
     }
 
-    Node* initializeHeap() {
-        Node* np {nullptr};
-        return np;
+    std::shared_ptr<Node> initializeHeap() {
+        return nullptr;
     }
 
-    void link(Node *h1, Node *y, Node* z) {
-        (y->left)->right = y->right;
-        (y->right)->left = y->left;
-        if (z->right == z)
-            h1 = z;
-        y->left = y;
-        y->right = y;
-        y->parent = z;
+    void link(std::shared_ptr<Node> root1, std::shared_ptr<Node> nodeToLink, std::shared_ptr<Node> root2) {
+        (nodeToLink->left)->right = nodeToLink->right;
+        (nodeToLink->right)->left = nodeToLink->left;
+        if (root2->right == root2)
+            root1 = root2;
+        nodeToLink->left = nodeToLink;
+        nodeToLink->right = nodeToLink;
+        nodeToLink->parent = root2;
 
-        if (z->child == nullptr)
-            z->child = y;
+        if (root2->child == nullptr)
+            root2->child = nodeToLink;
 
-        y->right = z->child;
-        y->left = (z->child)->left;
-        ((z->child)->left)->right = y;
-        (z->child)->left = y;
+        nodeToLink->right = root2->child;
+        nodeToLink->left = (root2->child)->left;
+        ((root2->child)->left)->right = nodeToLink;
+        (root2->child)->left = nodeToLink;
 
-        if (y->n < (z->child)->n)
-            z->child = y;
-        z->degree++;
+        if (nodeToLink->key < (root2->child)->key)
+            root2->child = nodeToLink;
+        root2->degree++;
     }
 
-    Node* createNode(int value) {
-        Node* x {new Node};
-        x->n = value;
-        return x;
+    std::shared_ptr<Node> createNode(int value) {
+        auto newNode = std::make_shared<Node>();
+        newNode->key = value;
+        return newNode;
     }
 
-    Node* insert(Node* H, Node* x) {
-       x->degree = 0;
-       x->parent = nullptr;
-       x->child = nullptr;
-       x->left = x;
-       x->right = x;
-       x->mark = 'F';
-       x->C = 'N';
-        if (H != nullptr) {
-            (H->left)->right = x;
-            x->right = H;
-            x->left = H->left;
-            H->left = x;
-            if (x->n < H->n)
-                H = x;
-        }  
+    std::shared_ptr<Node> insert(std::shared_ptr<Node> heapRoot, std::shared_ptr<Node> newNode) {
+        newNode->degree = 0;
+        newNode->parent = nullptr;
+        newNode->child = nullptr;
+        newNode->left = newNode;
+        newNode->right = newNode;
+        newNode->mark = 'F';
+        newNode->childCut = 'N';
+        if (heapRoot != nullptr) {
+            (heapRoot->left)->right = newNode;
+            newNode->right = heapRoot;
+            newNode->left = heapRoot->left;
+            heapRoot->left = newNode;
+            if (newNode->key < heapRoot->key)
+                heapRoot = newNode;
+        }
         else {
-            H = x;
-        }       
-        nH = nH + 1;
-        return H;
+            heapRoot = newNode;
+        }
+        heapSize = heapSize + 1;
+        return heapRoot;
     }
 
-    Node* Union(Node* H1, Node* H2) {
-        Node *np;
-        Node *H = initializeHeap();
-        H = H1;
-        (H->left)->right = H2;
-        (H2->left)->right = H;
-        np = H->left;
-        H->left = H2->left;
-        H2->left = np;
-        return H;
+    std::shared_ptr<Node> Union(std::shared_ptr<Node> heap1, std::shared_ptr<Node> heap2) {
+        std::shared_ptr<Node> nextPointer;
+        auto newHeap {initializeHeap()};
+        newHeap = heap1;
+        (newHeap->left)->right = heap2;
+        (heap2->left)->right = newHeap;
+        nextPointer = newHeap->left;
+        newHeap->left = heap2->left;
+        heap2->left = nextPointer;
+        return newHeap;
     }
 
-    Node* extractMin(Node* H1) {
-        Node *p;
-        Node *ptr;
-        Node *z = H1;
-        p = z;
-        ptr = z;
-        if (z == nullptr)
-            return z;
+    std::shared_ptr<Node> extractMin(std::shared_ptr<Node> heapRoot) {
+        std::shared_ptr<Node> current;
+        std::shared_ptr<Node> nodeToextract;
+        std::shared_ptr<Node> minNode {heapRoot};
+        current = minNode;
+        nodeToextract = minNode;
+        if (minNode == nullptr)
+            return minNode;
 
-        Node *x;
-        Node *np;
+        std::shared_ptr<Node> childNode;
+        std::shared_ptr<Node> nextNode;
 
-        x = nullptr;
+        childNode = nullptr;
 
-        if (z->child != nullptr)
-            x = z->child;
+        if (minNode->child != nullptr)
+            childNode = minNode->child;
 
-        if (x != nullptr)
-        {
-            ptr = x;
-            do  {
-                np = x->right;
-                (H1->left)->right = x;
-                x->right = H1;
-                x->left = H1->left;
-                H1->left = x;
-                if (x->n < H1->n)
-                    H1 = x;
+        if (childNode != nullptr) {
+            nodeToextract = childNode;
+            do {
+                nextNode = childNode->right;
+                (heapRoot->left)->right = childNode;
+                childNode->right = heapRoot;
+                childNode->left = heapRoot->left;
+                heapRoot->left = childNode;
+                if (childNode->key < heapRoot->key)
+                    heapRoot = childNode;
 
-                x->parent = nullptr;
-                x = np;
-            } while (np != ptr);
+                childNode->parent = nullptr;
+                childNode = nextNode;
+            } while (nextNode != nodeToextract);
         }
 
-        (z->left)->right = z->right;
-        (z->right)->left = z->left;
-        H1 = z->right;
+        (minNode->left)->right = minNode->right;
+        (minNode->right)->left = minNode->left;
+        heapRoot = minNode->right;
 
-        if (z == z->right && z->child == nullptr)
-            H = nullptr;
-
+        if (minNode == minNode->right && minNode->child == nullptr)
+            heapRoot = nullptr;
         else {
-            H1 = z->right;
-            consolidate(H1);
+            heapRoot = minNode->right;
+            consolidate(heapRoot);
         }
-        nH = nH - 1;
-        return p;
+        heapSize = heapSize - 1;
+        return current;
     }
 
-    void consolidate(Node* H1) 
-    {
-        int d, i;
-        float f = (log(nH)) / (log(2));
-        int D = f;
-        Node *A[100];
+    void consolidate(std::shared_ptr<Node> heapRoot) {
+        int degree, i;
+        float f {(log(heapSize)) / (log(2))};
+        int D {f};
+        std::shared_ptr<Node> A[100];
 
         for (i = 0; i <= D; i++)
-          A[i] = nullptr;
+            A[i] = nullptr;
 
-        Node *x = H1;
-        Node *y;
-        Node *np;
-        Node *pt = x;
+        std::shared_ptr<Node> currentNode = heapRoot;
+        std::shared_ptr<Node> tempNode;
+        std::shared_ptr<Node> nextNode;
+        std::shared_ptr<Node> pt = currentNode;
 
-        do
-        {
-          pt = pt->right;
+        do {
+            pt = pt->right;
+            degree = currentNode->degree;
 
-          d = x->degree;
+            while (A[degree] != nullptr) {
+                tempNode = A[degree];
 
-          while (A[d] != NULL)
+                if (currentNode->key > tempNode->key) {
+                    nextNode = currentNode;
+                    currentNode = tempNode;
+                    tempNode = nextNode;
+                }
 
-          {
-            y = A[d];
-
-            if (x->n > y->n)
-
-            {
-              np = x;
-
-              x = y;
-
-              y = np;
+                if (tempNode == heapRoot)
+                    heapRoot = currentNode;
+                link(heapRoot, tempNode, currentNode);
+                if (currentNode->right == currentNode)
+                    heapRoot = currentNode;
+                A[degree] = nullptr;
+                degree = degree + 1;
             }
 
-            if (y == H1)
-              H1 = x;
-            link(H1, y, x);
-            if (x->right == x)
-              H1 = x;
-            A[d] = NULL;
-            d = d + 1;
-          }
+            A[degree] = currentNode;
+            currentNode = currentNode->right;
 
-          A[d] = x;
-          x = x->right;
-
+        } while (currentNode != heapRoot);
+        heapRoot = nullptr;
+        for (int j = 0; j <= D; j++) {
+            if (A[j] != nullptr) {
+                A[j]->left = A[j];
+                A[j]->right = A[j];
+                if (heapRoot != nullptr) {
+                    (heapRoot->left)->right = A[j];
+                    A[j]->right = heapRoot;
+                    A[j]->left = heapRoot->left;
+                    heapRoot->left = A[j];
+                    if (A[j]->key < heapRoot->key)
+                        heapRoot = A[j];
+                }
+                else {
+                    heapRoot = A[j];
+                }
+                if (heapRoot == nullptr)
+                    heapRoot = A[j];
+                else if (A[j]->key < heapRoot->key)
+                    heapRoot = A[j];
+            }
         }
+    }
 
-        while (x != H1);
-        H = NULL;
-        for (int j = 0; j <= D; j++)
-        {
-          if (A[j] != NULL)
-          {
-            A[j]->left = A[j];
-            A[j]->right = A[j];
-            if (H != NULL)
-            {
-              (H->left)->right = A[j];
-              A[j]->right = H;
-              A[j]->left = H->left;
-              H->left = A[j];
-              if (A[j]->n < H->n)
-                H = A[j];
-            }
-            else
-            {
-              H = A[j];
-            }
-            if (H == NULL)
-              H = A[j];
-            else if (A[j]->n < H->n)
-              H = A[j];
-          }
-        }
-    } 
-
-    void display(Node *H)
-    {
-        Node *p = H;
-        if (p == nullptr)
-        {
+    void display(std::shared_ptr<Node> heapRoot) {
+        std::shared_ptr<Node> current = heapRoot;
+        if (current == nullptr) {
             std::cout << "The Heap is Empty" << '\n';
-            
         }
         std::cout << "The root nodes of Heap are: " << '\n';
 
-        do
-        {
-            std::cout << p->n;
-            p = p->right;
-            if (p != H)
-            {
+        do {
+            std::cout << current->key;
+            current = current->right;
+            if (current != heapRoot) {
                 std::cout << "-->";
             }
-        } while (p != H && p->right != nullptr);
+        } while (current != heapRoot && current->right != nullptr);
         std::cout << '\n';
-    }      
-
-    Node* find(Node* H, int k)
-    {
-        Node *x = H;
-        x->C = 'Y';
-        Node *p = nullptr;
-        if (x->n == k)
-        {
-            p = x;
-            x->C = 'N';
-            return p;
-        }
-
-        if (p == nullptr)
-        {
-            if (x->child != nullptr)
-                p = find(x->child, k);
-            if ((x->right)->C != 'Y')
-                p = find(x->right, k);
-        }
-
-        x->C = 'N';
-        return p;
     }
 
-    int decreaseKey(Node* H1, int x, int k)
-    {
-        Node *y;
-        if (H1 == nullptr)
-        {
-            std::cout << "The Heap is Empty" << '\n';
+    std::shared_ptr<Node> find(std::shared_ptr<Node> heapRoot, int k) {
+        std::shared_ptr<Node> current = heapRoot;
+        current->childCut = 'Y';
+        std::shared_ptr<Node> foundNode = nullptr;
+        if (current->key == k) {
+            foundNode = current;
+            current->childCut = 'N';
+            return foundNode;
+        }
+
+        if (foundNode == nullptr) {
+            if (current->child != nullptr)
+                foundNode = find(current->child, k);
+            if ((current->right)->childCut != 'Y')
+                foundNode = find(current->right, k);
+        }
+
+        current->childCut = 'N';
+        return foundNode;
+    }
+
+    int decreaseKey(std::shared_ptr<Node> heap_root, int x, int new_key) {
+        std::shared_ptr<Node> parent_node;
+        if (heap_root == nullptr) {
+            std::cout << "A Heap está Vazia" << '\n';
             return 0;
         }
-        Node *ptr = find(H1, x);
-        if (ptr == nullptr)
-        {
-            std::cout << "Node not found in the Heap" << '\n';
+        std::shared_ptr<Node> found_node = find(heap_root, x);
+        if (found_node == nullptr) {
+            std::cout << "Nó não encontrado na Heap" << '\n';
             return 1;
         }
 
-        if (ptr->n < k)
-        {
-            std::cout << "Entered key greater than current key" << '\n';
+        if (found_node->key < new_key) {
+            std::cout << "Chave inserida é maior que a chave atual" << '\n';
             return 0;
         }
-        ptr->n = k;
-        y = ptr->parent;
-        if (y != nullptr && ptr->n < y->n)
-        {
-            cut(H1, ptr, y);
-            cascaseCut(H1, y);
+        found_node->key = new_key;
+        parent_node = found_node->parent;
+        if (parent_node != nullptr && found_node->key < parent_node->key) {
+            cut(heap_root, found_node, parent_node);
+            cascadeCut(heap_root, parent_node);
         }
 
-        if (ptr->n < H->n)
-            H = ptr;
+        if (found_node->key < heap_root->key)
+            heap_root = found_node;
 
         return 0;
     }
 
-    int deleteKey(Node* H1, int k)
-    {
-        Node *np = NULL;
+    int deleteKey(std::shared_ptr<Node> heap_root, int k) {
+        std::shared_ptr<Node> node_pointer = nullptr;
         int t;
-        t = decreaseKey(H1, k, -5000);
+        t = decreaseKey(heap_root, k, -5000);
         if (!t)
-            np = extractMin(H);
-        if (np != NULL)
-            std::cout << "Key Deleted" << '\n';
+            node_pointer = extractMin(heap_root);
+        if (node_pointer != nullptr)
+            std::cout << "Chave Deletada" << '\n';
         else
-            std::cout << "Key not Deleted" << '\n';
+            std::cout << "Chave não Deletada" << '\n';
         return 0;
     }
 
-    void cut(Node* H1, Node* x, Node *y)
-    {
-        if (x == x->right)
-            y->child = nullptr;
-        (x->left)->right = x->right;
-        (x->right)->left = x->left;
-        if (x == y->child)
-            y->child = x->right;
-        y->degree = y->degree - 1;
-        x->right = x;
-        x->left = x;
-        (H1->left)->right = x;
-        x->right = H1;
-        x->left = H1->left;
-        H1->left = x;
-        x->parent = nullptr;
-        x->mark = 'F';
+    void cut(std::shared_ptr<Node> heap_root, std::shared_ptr<Node> node_to_cut, std::shared_ptr<Node> parent_node) {
+        if (node_to_cut == node_to_cut->right)
+            parent_node->child = nullptr;
+        (node_to_cut->left)->right = node_to_cut->right;
+        (node_to_cut->left)->left = node_to_cut->left;
+        if (node_to_cut == parent_node->child)
+            parent_node->child = node_to_cut->right;
+        parent_node->degree = parent_node->degree - 1;
+        node_to_cut->right = node_to_cut;
+        node_to_cut->left = node_to_cut;
+        (heap_root->left)->right = node_to_cut;
+        node_to_cut->right = heap_root;
+        node_to_cut->left = heap_root->left;
+        heap_root->left = node_to_cut;
+        node_to_cut->parent = nullptr;
+        node_to_cut->mark = 'F';
     }
 
-    void cascaseCut(Node* H1, Node *y)
-    {
-        Node *z = y->parent;
-        if (z != NULL)
-        {
-            if (y->mark == 'F')
-            {
-                y->mark = 'T';
+    void cascadeCut(std::shared_ptr<Node> heap_root, std::shared_ptr<Node> parent_node) {
+        std::shared_ptr<Node> grandparent_node = parent_node->parent;
+        if (grandparent_node != nullptr) {
+            if (parent_node->mark == 'F') {
+                parent_node->mark = 'T';
             }
-            else
-            {
-                cut(H1, y, z);
-                cascaseCut(H1, z);
+            else {
+                cut(heap_root, parent_node, grandparent_node);
+                cascadeCut(heap_root, grandparent_node);
             }
         }
     }
 
+private:
+    int heapSize;
+    std::shared_ptr<Node> heapRoot;
 };
